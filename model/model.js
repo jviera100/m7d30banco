c18d082e8bff0c3e421489028afec21ec69cd0f7
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-04-15 14:58:26 -0400

    :tada: probando

--- model/model.js
+++ model/model.js
@@ -1,23 +1,61 @@
-import { sequelize } from '../config/dbPool.js'; // Cambio en la ruta de importación
+// En el archivo usuarios.js
+import { sequelize } from '../config/dbPool.js';
 import { DataTypes } from 'sequelize';
 
-const Project = sequelize.define('project', {
+const Usuario = sequelize.define('usuario', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    name: {
-        type: DataTypes.STRING
+    nombre: {
+        type: DataTypes.STRING(50)
     },
-    priority: {
-        type: DataTypes.INTEGER
-    },
-    description: {
-        type: DataTypes.STRING
+    balance: {
+        type: DataTypes.FLOAT,
+        validate: {
+            min: 0 // Validación para que el balance sea mayor o igual a cero
+        }
     }
 }, {
-    timestamps: true
+    timestamps: false // No necesitas timestamps en esta tabla
 });
 
-export default Project;
+export { Usuario };
+
+// En el archivo transferencias.js
+import { sequelize } from '../config/dbPool.js';
+import { DataTypes } from 'sequelize';
+import { Usuario } from './usuarios.js'; // Importa el modelo de usuarios para la relación
+
+const Transferencia = sequelize.define('transferencia', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    emisorId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Usuario, // Aquí debes especificar el modelo de Usuario
+            key: 'id'
+        }
+    },
+    receptorId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Usuario, // Aquí también
+            key: 'id'
+        }
+    },
+    monto: {
+        type: DataTypes.FLOAT
+    },
+    fecha: {
+        type: DataTypes.DATE
+    }
+}, {
+    timestamps: false // No necesitas timestamps en esta tabla
+});
+
+export { Transferencia };
