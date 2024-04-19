3c6342c9da393e2e491e22c8eb2db93fea0e57eb
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-04-18 20:40:19 -0400

    :bug: type module no se agrega en el html o da error y tengo error en data monto

--- assets/js/principal/cambio-color-titulo-banco.js
+++ assets/js/principal/cambio-color-titulo-banco.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     //codigo de funcion se ejecuta cuando pagina este cargada
           
         // Cambia color titulo formulario super hero
-        $("#titulo-card").dblclick(function () 
+        $("#banco-titulo").dblclick(function () 
         {
             $(this).css("color", "red");
         });    
