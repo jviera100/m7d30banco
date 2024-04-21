c9a76be236819ca0fcd5dba03ad038895f05f66b
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-04-21 08:24:36 -0400

    :tada: depuracion y estilos

--- assets/js/animal/animal-autoejecutable.js
+++
@@ -1,4 +0,0 @@
-function autoejecutable() {
-    console.log("Trabajo terminado");
-}
-autoejecutable(); 
--- db/usuarios.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE usuarios (id SERIAL PRIMARY KEY, nombre VARCHAR(50),
-balance FLOAT CHECK (balance >= 0));
-
-select * from usuarios
